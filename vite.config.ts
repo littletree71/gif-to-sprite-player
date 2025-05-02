3e47fee004c1dc95047ce2ffe6601e1d7e25d0a7
littletree71 <eric8271@gmail.com>
2025-05-03 02:46:50 +0800

    feat: add configuration files and update package dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import path from "path"
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/components/GifToSpritePlayer/index.ts"),
+      name: "GifToSpritePlayer",
+      fileName: (format) => `gif-to-sprite-player.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"], // 不打包 Vue，作為外部依賴
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': path.resolve(new URL('.', import.meta.url).pathname, 'src')
     }
   }
 })
